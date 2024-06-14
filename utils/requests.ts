6d2106a702a9262bab9ea2fe740abe0cfda7cdd6
Ajay Sharma <ajaysharma12799@gmail.com>
2024-06-15 00:17:12 +0530

    utility request

---
+++ utils/requests.ts
@@ -0,0 +1,42 @@
+type FetchProperties = {
+  page_limit: number;
+  page_number: number;
+};
+
+type FetchPropertyById = {
+  id: string;
+};
+
+async function fetchProperties({ page_limit }: FetchProperties) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties?page_limit=${page_limit}`
+    );
+    const properties = await response.json();
+
+    if (!response.ok) {
+      throw new Error("Failed to Fetch Data");
+    }
+    return properties;
+  } catch (error) {
+    console.log("Fetch Properties Error --> ", error);
+  }
+}
+
+async function fetchPropertyById({ id }: FetchPropertyById) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties/${id}`
+    );
+    const property = await response.json();
+
+    if (!response.ok) {
+      throw new Error("Failed to Fetch Data");
+    }
+    return property;
+  } catch (error) {
+    console.log("Fetch Property By ID Error --> ", error);
+  }
+}
+
+export { fetchProperties, fetchPropertyById };
