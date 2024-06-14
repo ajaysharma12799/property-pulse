fc11da4f221c01bbfecefb6f94008e1fe9d48205
Ajay Sharma <ajaysharma12799@gmail.com>
2024-06-15 00:17:59 +0530

    User & Property model

---
+++ models/User.ts
@@ -0,0 +1,32 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        refs: "Property",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.user || model("User", UserSchema);
+
+export default User;
