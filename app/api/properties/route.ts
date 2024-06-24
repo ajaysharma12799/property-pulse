eb73e4eaa00e9ea25db25faa3b3aefe322f7bc66
Ajay Sharma <ajaysharma12799@gmail.com>
2024-06-25 00:49:36 +0530

    added POST route

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,6 +1,8 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getUserSession } from "@/utils/getUserSession";
+import cloudinary from "@/config/cloudinary";
 
 /**
  * Request: GET
@@ -36,6 +38,110 @@ export const GET = async (request: NextRequest) => {
       }
     );
   } catch (error) {
+    return Response.json(
+      {
+        status: false,
+        error: "Something Went Wrong",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
+
+/**
+ * Request: POST
+ * Path: /api/properties
+ */
+export const POST = async (request: NextRequest, response: NextResponse) => {
+  try {
+    // DB Connection
+    await connectDB();
+
+    const session = await getUserSession();
+
+    if (!session || !session.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = session;
+
+    const formData = await request.formData(); // If using Form-Data on frontend then use this format to access entire formData on Server
+
+    // Access all values from amenities and images
+    const amenities = formData.getAll("amenities");
+    const images = formData
+      .getAll("images")
+      .filter((image: any) => image.name !== "");
+
+    // Create Property Object
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+    };
+
+    // Upload Images to Cloudinary
+    const imageUploadPromise = [];
+
+    for (let image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageBuffer);
+
+      // Convert Image Data to Base64
+      const imageBase64 = imageData.toString("base64");
+
+      // Upload Image to Cloudinary
+      const results = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: "property-pulse",
+        }
+      );
+
+      imageUploadPromise.push(results.secure_url);
+
+      // Wait for Images to Upload
+      const uploadedImages = await Promise.all(imageUploadPromise);
+
+      // Add Uploaded Images to Property Data
+      propertyData.images = uploadedImages;
+    }
+
+    console.log("Data --> ", propertyData);
+
+    // Save Property
+    const property = await Property.create(propertyData);
+
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${property._id}`
+    );
+  } catch (error) {
+    console.log("POST ERROR --> ", error);
+
     return Response.json(
       {
         status: false,
